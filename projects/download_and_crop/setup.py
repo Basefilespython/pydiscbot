cb66df9a9492d8014867f7f041e9e18d7818d958
Basefilespython <123670499+Basefilespython@users.noreply.github.com>
2023-02-13 17:52:27 +0300

    Update setup.py

--- projects/download_and_crop/setup.py
+++ projects/download_and_crop/setup.py
@@ -1,17 +1,16 @@
 import requests
 import os
 
-name_dir = "NeKo_18+"
-one_path = os.getcwd()
+name_dir = "Dand_Crop"
 try:
-  os.chdir(f'os.mkdir("Dand_Crop")')
+  os.mkdir(name_dir)
+  os.chdir(name_dir)
+  one_path = os.getcwd()
 except FileExistsError:
   pass
 
 def download_file_from_github(file_name):
   try:
-    autor = 'Basefilespython'
-    dictgit = 'pydiscbot'
     url = f"https://raw.githubusercontent.com/Basefilespython/pydiscbot/main/projects/download_and_crop/{file_name}"
     def download_file(url):
       local_filename = url.split('/')[-1]
@@ -34,11 +33,10 @@ def update():
   for file_name in file_names:
     er = er + "\n" + file_name + "\n" + str(
       download_file_from_github(file_name))
-    #er = download_file_from_github(file_name)
-  #print(er)
-  import time
-  time.sleep(2)
-  return er
+
+#   import time
+#   time.sleep(2)
+  #return er
 
 update()
 with open("start.bat", "w") as outfile:

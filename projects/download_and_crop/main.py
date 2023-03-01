a0c4fda6bffc11eeff663ebc76ea8d2149e417ea
Basefilespython <123670499+Basefilespython@users.noreply.github.com>
2023-03-01 13:22:07 +0300

    Update main.py

--- projects/download_and_crop/main.py
+++ projects/download_and_crop/main.py
@@ -7,7 +7,7 @@ from urllib.request import HTTPError
 import requests
 
 s_version = "2.1.5"
-n_version = '0.1.6'
+
 
 
 def download_file_from_github(file_name):
@@ -43,7 +43,7 @@ def update():
   time.sleep(2)
 
 
-#update()
+update()
 
 black = "\033[30m"
 red = "\033[31m"

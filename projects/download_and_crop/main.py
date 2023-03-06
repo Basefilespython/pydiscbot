9570f31fd4a0ab054ad7a47b556be17aca78ed4a
Basefilespython <123670499+Basefilespython@users.noreply.github.com>
2023-03-06 07:09:19 +0300

    Add files via upload

--- projects/download_and_crop/main.py
+++ projects/download_and_crop/main.py
@@ -13,8 +13,9 @@ import requests
 
 def download_file_from_github(file_name):
   try:
-
-    url = f"https://raw.githubusercontent.com/Basefilespython/pydiscbot/main/{file_name}"
+    autor = 'Basefilespython'
+    dictgit = 'pydiscbot'
+    url = f"https://raw.githubusercontent.com/{autor}/{dictgit}/main/{file_name}"
 
     def download_file(url):
       local_filename = url.split('/')[-1]
